c270980f04b721b4aeee50e3daf650503aa17818
latina2kjh <ca7fullnganh3@gmail.com>
2025-03-15 07:31:59 +0700

    1

--- ttp/yamashiro-hs/mt/js/acc.js
+++
@@ -1,5 +0,0 @@
-//document.write("<img src='http://user.o0o0.jp/High_School/YAMASHIRO/acc/acclog.cgi?");
-//document.write("referrer="+document.referrer+"&");
-//document.write("width="+screen.width+"&");
-//document.write("height="+screen.height+"&");
-//document.write("color="+screen.colorDepth+"'>");
